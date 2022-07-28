239cb770558d3b786c6a56b03ad7fe389816bdf1
gweelstone <ericw1816@gmail.com>
2022-07-28 14:00:31 +0300

    project test

---
+++ src/components/HelloCircleCI.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>This is a test file for CircleCI.</h3>
+    <div>
+    <p>{{ greeting }}</p>
+    <input id="username" v-model="username" />
+    <button v-on:click="click1()">Click here</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloCircleCI',
+  props: {
+    msg: String,
+  },
+  data: function () {
+    return {
+      username: 'Nedashkivskyi',
+      greeting: 'Hello',
+    }
+  },
+  methods: {
+    click1: function () {
+      //console.log("click1 clicked.")
+      this.greeting = 'Hello, ' + this.username
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ tests/unit/HelloCircleCI.vue.spec.js
@@ -0,0 +1,29 @@
+import { shallowMount, mount } from '@vue/test-utils'
+import HelloCircleCI from '@/components/HelloCircleCI.vue'
+
+describe('HelloCircleCI.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloCircleCI, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+
+  it('change greeting message when click here button clicked', async () => {
+    const msg = 'new message'
+    const expected_greeting = 'Hello, circleci'
+    const wrapper = mount(HelloCircleCI, {
+      propsData: { msg }
+    })
+
+    let input_text = 'circleci'
+    wrapper.find("input").setValue(input_text)
+
+    wrapper.find("button").trigger('click')
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.find("p").text()).toBe(expected_greeting)
+    //expect(wrapper.vm.greeting).toMatch(expected_greeting)
+  })
+})
